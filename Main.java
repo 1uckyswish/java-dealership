68777b98472c10a5e0be2341d9ee1ffbdd2bacaa
Noel Guillen <noelblas274@gmail.com>
2024-02-27 00:57:37 -0500

    fixed mutation for arrays in setter and constructor

--- Main.java
+++ Main.java
@@ -1,16 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Car nissan = new Car("nissan", 10000, 2020, "Green");
-        Car dodge = new Car("Dodge", 11000, 2019, "Blue");
 
+        Car nissan = new Car("nissan", 10000, 2020, "Green", new String[] {"Tires", "Keys"});
+        Car dodge = new Car("Dodge", 11000, 2019, "Blue", new String[] {"Tires", "Keys"});
         Car nissan2 = new Car(nissan);
+
         nissan2.setColor("Yellow");
-
-
-
-        nissan.drive();
         nissan2.drive();
-        dodge.drive();
     }
 }
 
